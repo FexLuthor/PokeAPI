b8f28c27db1307c6f19b6e67be63fed0692906ea
FexLuthor <felixcrunge@gmail.com>
2024-04-18 18:50:58 +0200

    Neuer Versuch

--- PokeView/MainWindow.xaml.cs
+++ PokeView/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace PokeView
 {
@@ -24,11 +26,55 @@ namespace PokeView
         }
 
 
-        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        private async void SearchButton_Click(object sender, RoutedEventArgs e)
         {
-            Pokemon = PokeInput.Text;
-            PokeName.Content = Pokemon;
+            SystemMessage.Text = "";
+            Pokemon = PokeInput.Text.ToLower();
+            PokeModel PokemonDat = new PokeModel();
+            PokemonDat = await Request.PokeCall(Pokemon);
+            if (PokemonDat.OnlineSuccess)
+            {
+
+                PokeName.Content = Pokemon.ToUpper();
+
+                for (int i = 0; i < PokemonDat.Type.Count; i++)
+                {
+                    PokeType.Text += PokemonDat.Type[i];
+                    if (i < PokemonDat.Type.Count - 1)
+                    {
+                        PokeType.Text += ", ";
+                    }
+                }
+
+                for (int i = 0; i < PokemonDat.Ability.Count; i++)
+                {
+                    PokeAbilities.Text += PokemonDat.Ability[i];
+
+                    if (i < PokemonDat.Ability.Count - 1)
+                    {
+                        PokeAbilities.Text += ", ";
+                    }
+                }
+                Image BildShiny = new Image();
+                BildShiny.Source = new BitmapImage(new Uri(PokemonDat.ShinyUrl));
+                PokeShiny.Source = BildShiny.Source;
+
+                Image BildNormal = new Image();
+                BildNormal.Source = new BitmapImage(new Uri(PokemonDat.PictureUrl));
+                PokeNormal.Source = BildNormal.Source;
+
+               
+
+
+
+
+            }
+            else
+            {
+                SystemMessage.Text = "Pokémon not found. Please check and reenter your Pokémons name.";
+            }
             PokeInput.Clear();
+
         }
     }
 }
